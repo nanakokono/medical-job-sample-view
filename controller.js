c248e937e9660708bdfe65ee1f2645937fbcadbc
“Nanako <nanako.kono@bizreach.co.jp>
2019-05-03 17:23:37 +0900

    （SP）求人詳細の応募ボタンFIX実装

--- controller.js
+++ controller.js
@@ -1,10 +1,15 @@
 $(function(){
+  const FORM_PADDING = 40;
+  const SP_BREAK_POINT = 800;
+  const $isPC = $(window).width() > SP_BREAK_POINT;
+
+  $(window).scroll(function () {
     const $jobDetailDescriptionTop = $('#job-detail-description').offset().top;
-    const $jobDetailFormTop = $('#job-detail-form').offset().top;
+    const $jobDetailFormTop = $('#job-detail-form').offset().top - FORM_PADDING;
     const $jobDetailFixed = $('#job-detail-fixed');
     const $jobDetailFixedButton = $('#job-detail-fixed-button');
 
-    $(window).scroll(function () {
+    if($isPC) {
       if($(window).scrollTop() > $jobDetailDescriptionTop) {
         $jobDetailFixed.fadeIn();
       }
@@ -22,5 +27,15 @@ $(function(){
         $jobDetailFixedButton.removeClass('disabled');
         $jobDetailFixedButton.attr('href', '#job-detail-form')
       }
-    })
+    } else {
+      const $jobDetailFixedButtonSp = $('#job-detail-fixed-button-sp');
+      if($(window).scrollTop() >= $jobDetailFormTop) {
+        $jobDetailFixedButtonSp.fadeOut();
+      }
+
+      if($(window).scrollTop() < $jobDetailFormTop) {
+        $jobDetailFixedButtonSp.fadeIn();
+      }
+    }
+  })
 });
